21d8420e095a5073a8689c637877ee328b0efb7c
maty_mboup <matyseckmboup@esp.sn>
2023-10-24 14:50:51 +0000

    third commit

--- src/test/java/com/example/rh/rhapi/RhApiApplicationTests.java
+++ src/test/java/com/example/rh/rhapi/RhApiApplicationTests.java
@@ -1,72 +1,34 @@
 package com.example.rh.rhapi;
 
+import com.example.rh.rhapi.controller.EmployeController;
 import com.example.rh.rhapi.model.Employe;
-import com.example.rh.rhapi.repository.EmployeRepository;
+
+import com.example.rh.rhapi.service.EmployeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers= EmployeController.class)
+
 class RhApiApplicationTests {
+    @Autowired
+    private MockMvc myMock;
+    @MockBean
+    private EmployeService employeService;
 
+    @Test
+    public void testGetEmployees() throws Exception {
+      myMock.perform(get("/employees")).andExpect((status().isOk()));
+    }
 
-//	@Test
-//	void contextLoads() {
-//	}
-	/*@Test
-	public void testAjoutEmploye(){
-		Employe E1= new Employe("Arame", "Mboup","mboup0@gmail.com","arame1234");
-		//Employe E2 = new Employe("Maty", "Mboup","mboup0@gmail.com","pa1234");
-
-		employeRepos.save(E1);
-		//employeRepos.save(E2);
-
-		System.out.println(E1);
-		//System.out.println(E2);
-
-
-	}
-	/*
-	@Test
-	public void testFindEmploye(){
-		Employe a = employeRepos.findById(2L).get();
-		System.out.println(a);
-
-	}
-	@Test
-	public void nombreEmploye(){
-		long x= employeRepos.count();
-		System.out.println(x);
-	}
-	@Test
-	public void testFindAllEmployes(){
-		List <Employe> a = (List<Employe>) employeRepos.findAll();
-		for (Employe p:a){
-			System.out.println(p);
-		}
-
-
-	}
-	@Test
-	public void testUpdateEmploye(){
-		Employe a = employeRepos.findById(3L).get();
-		a.setPrenom("Fatou");
-		a.setNomDeFamille("Fall");
-		a.setMail("Fatou@gmail.com");
-		a.setPassword("Fatou1234");
-		employeRepos.save(a);
-		System.out.println(a);
-
-	}
-	@Test
-	public void testDeleteEmploye(){
-		employeRepos.deleteById(4L);
-		System.out.println("supression reussi");
-
-
-	}
-
-*/
 }
+
